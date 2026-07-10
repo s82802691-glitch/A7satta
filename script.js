7f1ba79f8b93a909768534f4c3b2a48aa9da3ad1
s82802691-glitch <s82802691@gmail.com>
2026-07-10 13:17:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+// =========================
+// LIVE CLOCK
+// =========================
 
+function updateClock() {
+
+const now = new Date();
+
+const options = {
+weekday:'long',
+day:'2-digit',
+month:'long',
+year:'numeric'
+};
+
+const date = now.toLocaleDateString('en-IN', options);
+
+const time = now.toLocaleTimeString('en-IN');
+
+const clock = document.getElementById("liveClock");
+
+if(clock){
+
+clock.innerHTML = `
+${date}<br>
+${time}
+`;
+
+}
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
+
+
+// =========================
+// AUTO REFRESH
+// =========================
+
+setInterval(function(){
+
+location.reload();
+
+},10000);
+
+
+// =========================
+// SAMPLE LIVE RESULT
+// =========================
+
+document.getElementById("marketName").innerHTML="Loading...";
+
+document.getElementById("marketResult").innerHTML="XX";
